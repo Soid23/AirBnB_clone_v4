95b801b23e8d9751ce85934f28a6a803ab479d5f
Soid23 <dorrismutahi@gmail.com>
2024-05-05 20:55:14 +0300

    adds to v4

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  const amens = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amens[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amens[$(this).data('id')];
+      }
+    });
+    if (Object.values(amens).length > 0) {
+      $('.amenities h4').text(Object.values(amens).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET',
+    success: function(data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+});
